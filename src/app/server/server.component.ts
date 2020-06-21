d58d515c71fe028fac1034b9ac9792e186ab30eb
srivardhan20 <67057863+srivardhan20@users.noreply.github.com>
2020-06-21 17:46:12 +0530

    Two way databinding.Directives ngIf,ngFor,ngStyle,ngClass,ngModel
    Code example for Angular Features: Two way databinding.Directives ngIf,ngFor,ngStyle,ngClass,ngModel
    Related assignment solution done

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,13 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: [`
+    .online{
+    color: white;}
+    `]
 })
-export class ServerComponent{
-serverId: number = 10;
-serverStatus: String = 'offline'; 
+export class ServerComponent {
+    serverId: number = 10;
+    serverStatus: String = 'offline';
 
-getServerStatus(){
-    return this.serverStatus; 
-}
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'Online' : 'offline';
+    }
+
+    getServerStatus() {
+        return this.serverStatus;
+    }
+
+    getColor(){
+        return this.serverStatus === "Online" ? "green" : "red";
+    }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,25 +6,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-allowNewServer = false;
-serverCreationStatus = "No Server Created!";
-serverName = "";
-  constructor() { 
+  allowNewServer = false;
+  serverCreationStatus = "No Server Created!";
+  serverName = "TestServerName";
+  userName = "";
+  serverCreated = false;
+  servers = ["New Server", "New Server 2"];
+  displayDetails = false;
+  displayButtonClick = 0;
+  displayDetailsArray = [];
+
+  constructor() {
     setTimeout(() => {
-    this.allowNewServer = true;
+      this.allowNewServer = true;
     }
-    ,2000);
+      , 2000);
   }
 
   ngOnInit(): void {
   }
 
-  onCreateServer(){
-    this.serverCreationStatus = "Server was created";
+  onCreateServer() {
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = "Server was created, name " + this.serverName;
+    this.serverCreated = true;
   }
 
-  onUpdateServerName(event: Event){
+  onUpdateServerName(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value;
   }
 
+
+  setDefaultUserName() {
+    this.userName = "";
+  }
+
+  toggleDisplayDetails() {
+    if (!this.displayDetails) {
+      this.displayDetails = true;
+    } else {
+      this.displayDetails = false;
+    }
+    this.displayDetailsArray.push(new Date());
+  }
+
+  getDisplayColor(){
+    if(this.displayDetailsArray.length>4){
+      return "blue";
+   }
+   return "";
+  }
 }
